06306f0eb583378c49793472030d885d6fdce374
heart-bool <18908069164@163.com>
2017-03-12 23:05:58 +0800

    集成 redis 缓存

--- src/main/java/com/feng/web/App.java
+++ src/main/java/com/feng/web/App.java
@@ -2,13 +2,10 @@ package com.feng.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Hello world!
- */
-
-@Configuration
+@EnableCaching
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
