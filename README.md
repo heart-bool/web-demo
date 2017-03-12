7e6bc80066b332036730f4bad3fd33f51535e478
heart-bool <18908069164@163.com>
2017-03-12 22:47:17 +0800

    修改注释

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web-demo
-demo
+这是一个使用Spring Boot 搭建的 java web 项目骨架
+已集成mybatis 
+maven assembly 打包插件 
+
+目的是为了测试和使用 GitHub， 别无他意
---
+++ src/test/java/com/feng/web/Test.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/3/12.
+ */
+
+package com.feng.web;
+
+/**
+ * Description:
+ *
+ * @Author: Heart_Bool / FENG WANG
+ * @Email: <email>18908069164@163.com</email>
+ * @Date: 2017/3/12 22:41
+ */
+public class Test {
+}
