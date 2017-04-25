7c06af7177efae3a70adaafc1f8a520b8a47daad
heart-bool <18908069164@163.com>
2017-04-26 01:37:16 +0800

    合并代码

--- src/main/java/com/feng/web/service/UserService.java
+++ src/main/java/com/feng/web/service/UserService.java
@@ -1,6 +1,6 @@
 package com.feng.web.service;
 
-import com.feng.web.domain.User;
+import com.feng.web.domain.user.User;
 import com.feng.web.mapper.user.UserMapper;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -12,8 +12,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
